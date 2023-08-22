25b11ca7a7263953ed057fb8e48267863d8fa512
John Carges <johncarges@gmail.com>
2023-08-22 12:45:25 -0400

    defines init and get set for dog and person classes

--- lib/dog.py
+++ lib/dog.py
@@ -12,4 +12,44 @@ APPROVED_BREEDS = [
 ]
 
 class Dog:
+
+    def __init__(self, name="Pooch", breed="Mastiff"):
+        if (type(name)== str) and (1 <= len(name) <= 25):
+            self.name = name
+        else:
+            print("Name must be string between 1 and 25 characters.")
+        
+        if breed in APPROVED_BREEDS:
+            self.breed = breed
+        else:
+            print("Breed must be in list of approved breeds.")
+        
+
+    def get_name(self):
+        print("Retrieving name.")
+        return self._name
+
+    def set_name(self, name):
+        if (type(name)== str) and (1 <= len(name) <= 25):
+            self._name = name
+            print(self._name)
+        else:
+            print("Name must be string between 1 and 25 characters.")
+
+    def get_breed(self):
+        print("Retrieving breed.")
+        return self._breed
+    
+    def set_breed(self, breed):
+        if breed in APPROVED_BREEDS:
+            self._breed = breed
+        else:
+            print("Breed must be in list of approved breeds.")
+
+
+
     pass
+
+fido = Dog("Fido")
+fido.set_name("")
+print(fido.name)
--- lib/person.py
+++ lib/person.py
@@ -16,4 +16,18 @@ APPROVED_JOBS = [
 ]
 
 class Person:
+
+    def __init__(self, name="John", job="Admin"):
+        if (type(name)== str) and (1 <= len(name) <= 25):
+            self.name = name.title()
+        else:
+            print("Name must be string between 1 and 25 characters.")
+        if job in APPROVED_JOBS:
+            self.job = job
+        else:
+            print("Job must be in list of approved jobs.")
+
+
+
+
     pass
